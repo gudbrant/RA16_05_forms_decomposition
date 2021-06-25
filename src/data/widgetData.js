55a41548a9ae22ef84e98d5cd38fb89e8fe88319
gudbrant <gudbrant@gmail.com>
2021-06-25 22:16:03 +0800

    Create widgetData.js

---
+++ src/data/widgetData.js
@@ -0,0 +1,71 @@
+export const popular = [
+  { title: "Практикум", subtitle: "— стать аналитиком" },
+  { title: "Яндекс.Браузер", subtitle: "— с переводом страниц" },
+  { title: "Недвижимость", subtitle: "— жилье с евроремонтом" }
+];
+
+export const tv = [
+  { time: "10:55", title: "Пятый канал", channel: "Улицы разбитых фонарей. 2-я серия" },
+  { time: "11:20", title: "Россия 24", channel: "Бегасон TV" },
+  { time: "12:45", title: "РЕН ТВ", channel: 'Казино "Рояль" ' }
+];
+
+export const podcasts = [
+  { title: "Управление как искусство", station: "Успех" },
+  { title: "Ночь. Мир в это время", station: "earthTV" },
+  { title: "Андрей Возн...", station: "Совершенно Секретно" }
+];
+
+export const rates = [
+  { title: "USD MOEX", rate: "75,49", growth: "−0,76" },
+  { title: "EUR MOEX", rate: "91,03", growth: "−1,23" },
+  { title: "НЕФТЬ", rate: "35,03", growth: "+17,31%" }
+];
+
+export const mainLinks = ["Сейчас в СМИ", "в Москве", "Интересное"];
+
+export const searchLinks = [
+  "Видео",
+  "Картинки",
+  "Новости",
+  "Карты",
+  "Маркет",
+  "Переводчик",
+  "Эфир"
+];
+
+export const news = [
+  {
+    text: "МИД Швеции вызвал посла России из-за ответных санкций",
+    sourceIcon: "pastafarianism"
+  },
+  {
+    text: "Путин освободил Барышева от должности замглавы МЧС России",
+    sourceIcon: "pepper hot"
+  },
+  {
+    text: "Генерал Реймонд: Россия разработала оружие для уничтожения спутников США",
+    sourceIcon: "flag usa"
+  },
+  {
+    text: "Главврач больницы в Коммунарке рассказал о возрасте больных COVID-19",
+    sourceIcon: "rocket"
+  },
+  {
+    text: "Захарова заявила о подрыве США своей консульской деятельности в России",
+    sourceIcon: "pepper hot"
+  }
+];
+
+export const searchExample = "текст песни сияй";
+
+export const promoItem = {
+  title: "Работа над ошибками",
+  desc: "Смотрите на Яндексе и запоминайте",
+  img: "https://www.equatorial.by/sites/default/files/banff-national-park001_2.jpg"
+};
+
+export const currentWeather = {
+  day: 18,
+  night: 4
+};
